eeed3f3070dd91921b815f2971447ebfcd734dd6
Connor Smith <connor.s1110@gmail.com>
2021-11-11 16:47:30 -0500

    Customer is asked to use gift cards on purchase

--- src/Customer.java
+++ src/Customer.java
@@ -205,61 +205,61 @@ public class Customer extends Throwable {
 		
 	}
 	
-	public int getRewardType(String custid) throws Exception {
-
-		Scanner op = new Scanner(System.in);
-		props = readPropertiesFile();
-		home = new Home();
-		con = db.getConnection();
-		String rewardName = null;
-
-		System.out.println("1. Purchase");
-		System.out.println("2. Leave a review");
-		System.out.println("3. Refer a friend");
-		System.out.println("4. Go back");
-		System.out.print("Your Option : ");
-
-		Home home = new Home();
-		Customer cust = new Customer();
-		int userop = op.nextInt();
-
-		try {
-			switch (userop) {
-			case 1:
-				rewardName = "Purchase";
-				//home.purchase(custid);
-				break;
-			case 2:
-				rewardName = "Leave a review";
-				//home.leaveReview();
-				break;
-			case 3:
-				rewardName = "Refer a friend";
-				//home.referFriend();
-				break;
-			case 4:
-				System.out.println("Go back");
-				//home.customerLanding(custid);
-				break;
-			default:
-				System.out.println("You have entered an invalid option");
-				getLPjoinedToRewardActivities(custid);
-			}
-
-			op.close();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		String getRewardTypeIdByName = props.getProperty("getRewardTypeIdByName").toString();
-		PreparedStatement statement = con.prepareStatement(getRewardTypeIdByName);
-		statement.setString(1, rewardName);
-		ResultSet rs = statement.executeQuery();
-		rs.next();
-		return rs.getInt("rewid");
-
-	}
+//	public int getRewardType(String custid) throws Exception {
+//
+//		Scanner op = new Scanner(System.in);
+//		props = readPropertiesFile();
+//		home = new Home();
+//		con = db.getConnection();
+//		String rewardName = null;
+//
+//		System.out.println("1. Purchase");
+//		System.out.println("2. Leave a review");
+//		System.out.println("3. Refer a friend");
+//		System.out.println("4. Go back");
+//		System.out.print("Your Option : ");
+//
+//		Home home = new Home();
+//		Customer cust = new Customer();
+//		int userop = op.nextInt();
+//
+//		try {
+//			switch (userop) {
+//			case 1:
+//				rewardName = "Purchase";
+//				//home.purchase(custid);
+//				break;
+//			case 2:
+//				rewardName = "Leave a review";
+//				//home.leaveReview();
+//				break;
+//			case 3:
+//				rewardName = "Refer a friend";
+//				//home.referFriend();
+//				break;
+//			case 4:
+//				System.out.println("Go back");
+//				//home.customerLanding(custid);
+//				break;
+//			default:
+//				System.out.println("You have entered an invalid option");
+//				getLPjoinedToRewardActivities(custid);
+//			}
+//
+//			op.close();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//		String getRewardTypeIdByName = props.getProperty("getRewardTypeIdByName").toString();
+//		PreparedStatement statement = con.prepareStatement(getRewardTypeIdByName);
+//		statement.setString(1, rewardName);
+//		ResultSet rs = statement.executeQuery();
+//		rs.next();
+//		return rs.getInt("rewid");
+//
+//	}
 
 	public void earnPoints(int custid, int lpid, String acttype) throws Exception {
 
@@ -267,7 +267,7 @@ public class Customer extends Throwable {
         Connection con = db.getConnection();
         Scanner op1 = new Scanner(System.in);
         
-        Connection conn = db.getConnection();
+        takeActivityInput(acttype, custid, lpid);
         PreparedStatement statement = con.prepareStatement("Select t.repoints from rerules t, activitytype at where t.lpid = ? and at.actname = ?");
 		statement.setInt(1, lpid);
 		statement.setString(2, acttype);
@@ -277,7 +277,7 @@ public class Customer extends Throwable {
 		int points = rs.getInt("repoints");
 		
 		PreparedStatement statement1;
-		statement1 = conn.prepareStatement("SELECT points, tier from WALLET where cid = ? and lpid = ?");
+		statement1 = con.prepareStatement("SELECT points, tier, num_cust_gc from WALLET where cid = ? and lpid = ?");
 		statement1.setInt(1, custid);
 		statement1.setInt(2, lpid);
 		
@@ -286,8 +286,31 @@ public class Customer extends Throwable {
 
 		int custpoints = rs2.getInt("points");
 		int cust_tier = rs2.getInt("tier");
+		int num_cust_gc = rs2.getInt("num_cust_gc");
+        
+        if (acttype.equalsIgnoreCase(acttype)) {
+        	while(true) {
+        		System.out.print("How many gift cards do you want to use: ");
+                int gc_amount = op1.nextInt();
+                
+                if (gc_amount <= num_cust_gc) {
+                	num_cust_gc -= gc_amount;
+                	statement1 = con.prepareStatement("INSERT INTO PURCHASE(custid, lpid, giftcardused, purchase_date) values(?, ?, ?, (SELECT SYSDATE FROM DUAL))");
+            		statement1.setInt(1, custid);
+            		statement1.setInt(2, lpid);
+            		statement1.setInt(3, gc_amount);
+            		
+            		rs2 = statement1.executeQuery();
+            		rs2.next();
+                	break;
+                }
+                else {
+                	System.out.println("You do not have enough gift cards");
+                }
+        	}
+        }
 		
-		statement1 = conn.prepareStatement("SELECT lptype from LOYALTYPROGRAM where lpid = ?");
+		statement1 = con.prepareStatement("SELECT lptype from LOYALTYPROGRAM where lpid = ?");
 		statement1.setInt(1, lpid);
 		ResultSet rs3 = statement1.executeQuery();
 		rs3.next();
@@ -298,7 +321,7 @@ public class Customer extends Throwable {
 		String lp_type = rs3.getString("lptype");
 		
 		if (lp_type == "tiered") {
-			statement1 = conn.prepareStatement("SELECT multiplier from BRAND_TIER where lpid = ? and tier_num = ?");
+			statement1 = con.prepareStatement("SELECT multiplier from BRAND_TIER where lpid = ? and tier_num = ?");
 			statement1.setInt(1, lpid);
 			statement1.setInt(2, cust_tier);
 			ResultSet rs4 = statement1.executeQuery();
@@ -307,7 +330,7 @@ public class Customer extends Throwable {
 			multiplier = rs4.getInt("multiplier");
 			totalpts = custpoints + (points * multiplier);
 			
-			statement1 = conn.prepareStatement("SELECT tier,points from BRAND_TIER where lpid = ? and tier_num >= ?");
+			statement1 = con.prepareStatement("SELECT tier,points from BRAND_TIER where lpid = ? and tier_num >= ?");
 			statement1.setInt(1, lpid);
 			statement1.setInt(2, cust_tier);
 			ResultSet rs5 = statement1.executeQuery();
@@ -325,11 +348,12 @@ public class Customer extends Throwable {
 
 		totalpts = custpoints + (points * multiplier);
 
-		statement1 = conn.prepareStatement("UPDATE WALLET set points = ?, tier = ? where cid = ? and lpid = ?");
+		statement1 = con.prepareStatement("UPDATE WALLET set points = ?, tier = ?, num_cust_gc = ? where cid = ? and lpid = ?");
 		statement1.setInt(1, totalpts);
 		statement1.setInt(2, new_cust_tier);
-		statement1.setInt(3, custid);
-		statement1.setInt(4, lpid);
+		statement1.setInt(3, num_cust_gc);
+		statement1.setInt(4, custid);
+		statement1.setInt(5, lpid);
 
 		statement1.executeUpdate();
 		
@@ -340,6 +364,37 @@ public class Customer extends Throwable {
 
 	}
 	
+	public void takeActivityInput(String acttype, int custid, int lpid) throws Exception {
+		DBConnector db = new DBConnector();
+        Connection con = db.getConnection();
+		Scanner op = new Scanner(System.in);
+		String result = "";
+		if (acttype.equalsIgnoreCase("write review")) {
+			System.out.print("Enter your review: ");
+			result = op.nextLine();
+	        
+	        PreparedStatement statement = con.prepareStatement("INSERT INTO LEAVE_REVIEW(CID, LPID, REVIEW_CONTENT) values(?, ?, ?)");
+			statement.setInt(1, custid);
+			statement.setInt(2, lpid);
+			statement.setString(3, result);
+			
+			ResultSet rs = statement.executeQuery();
+			rs.next();
+		}
+		else if (acttype.equalsIgnoreCase("refer friend")) {
+			System.out.print("Enter the name of the friend you want to refer: ");
+			result = op.nextLine();
+			
+			PreparedStatement statement = con.prepareStatement("INSERT INTO REFER_FRIEND(CID, LPID, REVIEW_CONTENT) values(?, ?, ?)");
+			statement.setInt(1, custid);
+			statement.setInt(2, lpid);
+			statement.setString(3, result);
+			
+			ResultSet rs = statement.executeQuery();
+			rs.next();
+		}
+}
+
 	public void redeemPoints(int custid, int lpid, String rewtype) throws Exception {
 
 		DBConnector db = new DBConnector();
